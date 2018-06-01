344ee48d6430fb7eba56065027a2a30d60bc27b6
ics-ikeda <ikeda@ics-web.jp>
2018-06-01 14:52:10 +0900

    npm方式に変更

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { DemoIconsPreload } from "./DemoIcons_4000";
+import "./style.css";
+
+window.addEventListener("load", () => {
+  new DemoIconsPreload();
+});
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'development',
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: './src/index.ts',
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader'
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {url: false}},
+        ],
+      },
+    ]
+  },
+  // import 文で .ts ファイルを解決するため
+  resolve: {
+    extensions: [
+      '.ts', '.js'
+    ],
+  },
+  devtool: 'source-map',
+  // webpack-serve の設定
+  serve: {
+    open: true,
+    content: './dist'
+  }
+};
