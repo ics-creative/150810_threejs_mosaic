fcf87abc628c097c3098648390606011d09334fd
ics-ikeda <ikeda@ics-web.jp>
2025-04-18 22:32:06 +0900

    gifを使っていないので

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -4,12 +4,6 @@ declare module "*.png" {
   export = value;
 }
 
-declare module "*.gif" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const value: any;
-  export = value;
-}
-
 declare module "*.jpg" {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const value: any;
