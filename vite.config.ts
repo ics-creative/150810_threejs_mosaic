2ab4654dffbe0834b89a744ff7738810d5e82f05
ics-ikeda <ikeda@ics-web.jp>
2025-04-18 20:33:41 +0900

    webpack → Vite

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // ビルド成果物の出力先ディレクトリ (プロジェクトルートからの相対パス)
+  build: {
+    outDir: "./docs", // ビルド成果物をプロジェクトルートの 'docs' に出力
+    rollupOptions: {
+      input: {
+        // エントリーポイントとなる各HTMLファイルへのパスを指定
+        // __dirname は vite.config.ts がある docs ディレクトリを指す
+        // そのため resolve(__dirname, 'ファイル名') で docs/ファイル名 を指定できる
+        // もし index.html が存在しない場合は、この行は削除してください
+        main: resolve(__dirname, "index.html"),
+        demoIcons: resolve(__dirname, "DemoIcons.html"),
+        demoCubes: resolve(__dirname, "DemoCubes.html"),
+        demoIcons_4000: resolve(__dirname, "DemoIcons_4000.html"),
+      },
+    }
+  },
+  // 開発サーバーの設定
+  server: {
+    open: true, // サーバー起動時にブラウザを自動で開く
+  },
+});
